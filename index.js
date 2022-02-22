22107ef692ef95c56d00f2aed453fd85522bd4d8
Kayci Buchanan <Kaycib05@gmail.com>
2022-02-22 09:23:40 -0500

    added new API key and changed small text color
    Signed-off-by: Kayci Buchanan <Kaycib05@gmail.com>

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ search.addEventListener("submit", event => {
     event.preventDefault();
     const text = input.value
     let urlEncodedText = encodeURIComponent(text);
-    fetch("http://www.omdbapi.com/?apikey=59354c85&s=" + urlEncodedText)
+    fetch("http://www.omdbapi.com/?apikey=ff4ee3df&s=" + urlEncodedText)
         // same thing as return response.json()
         .then(response => response.json())
         .then(data => {
